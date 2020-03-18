e1dbcd2490f5d6daa3b7ed6b6fea7b433d538690
matheus <matheus.sunderhus@gmail.com>
2020-03-17 23:17:55 -0300

    Feature(ServiceWorkers): - Adiciona imagens para PWA - Adiciona manifest na root - registra Service Worker para cache Feature(Servicos): - Cria templates de paginas de servicos
    Refactore(Home):
    - Ajuste links de manifest

---
+++ js/sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', function (event) {
+    console.log('service instaled');
+    event.waitUntil(
+        caches.open('static')
+        .then((cache) => {
+            cache.addAll([
+                '/Soluziona/',
+                '/Soluziona/js/main.js',
+                '/Soluziona/js/glightbox.min.js',
+                '/Soluziona/assets/img/soluzionaSplash144x144.png',
+                '/Soluziona/assets/img/caminhoes_trabalhando.png',
+                '/Soluziona/index.html',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap'
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('activate', function () {
+    console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (resp) {
+            if (resp) {
+                return resp;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
+});
