18a015956a7690e2483f3d4a0cd9208be2246c3f
matheus <matheus.sunderhus@gmail.com>
2020-03-18 19:21:29 -0300

    feature(Servicos): - Criado accordion de servicos - criado banner de call to action - criada animação do footer com api nativa do html - Adicionada  imagens necessárias par ao layout.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,31 @@ const toggleIdiomas = () => {
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('js/sw.js').then(
         function () {
-            console.log('register sw')
+            console.log('%c register sw', 'color:yellow; background-color:black;')
         }
     );
 }
+/* Toggle  accordion */
+const toggleAccordion = (item) => {
+    item.classList.toggle('active');
+    let descricao = item.nextElementSibling;
+    descricao.classList.toggle('active');
+}
+const accordionElements = document.querySelectorAll('.accordion-titulo');
+accordionElements.forEach(item => {
+    item.addEventListener('click', () => {
+        toggleAccordion(item)
+    })
+});
+
+
+/* animate footer detail*/
+let detailFooter = document.querySelector('.detalhe-footer img');
+window.addEventListener('deviceorientation', (event) => {
+    let orientation = window.screen.orientation;
+    if (orientation.type === "landscape-primary") {
+        detailFooter.style.transform = `translateX(${Math.floor(event.beta)*-3.5}px)`;
+    } else {
+        detailFooter.style.transform = `translateX(${Math.floor(event.gamma)*-2.5}px)`;
+    }
+});
